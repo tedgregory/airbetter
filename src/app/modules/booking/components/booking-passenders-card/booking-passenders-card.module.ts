6f4e7a499b52a3f8e632fcc41c1bd26e8c78e44b
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-04-24 06:08:44 +0300

    fix: edit passenger classnames, feat: create passengers card fields

--- src/app/modules/booking/components/booking-passenders-card/booking-passenders-card.module.ts
+++ src/app/modules/booking/components/booking-passenders-card/booking-passenders-card.module.ts
@@ -2,11 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingPassendersCardComponent } from './booking-passenders-card.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [BookingPassendersCardComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatButtonToggleModule,
+  ],
   exports: [BookingPassendersCardComponent],
 })
 export class BookingPassendersCardModule {}
