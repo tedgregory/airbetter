3db729b54c7d950a36c41eae75464efe49589bc5
Elena Markova <webkwondo.com@gmail.com>
2023-04-29 18:46:23 +0300

    feat: add footer styling based on #10

--- src/app/ui/theme/layout/components/footer/footer.module.ts
+++ src/app/ui/theme/layout/components/footer/footer.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ContainerModule } from 'src/app/ui/container/container.module';
 import { GridModule } from 'src/app/ui/grid/grid.module';
-import { PaymentIconsModule } from 'src/app/ui/payment-icons/payment-icons.module';
 import { FooterComponent } from './footer.component';
-import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [PaymentIconsModule, MatButtonModule, ContainerModule, GridModule],
+  imports: [ContainerModule, GridModule],
   declarations: [FooterComponent],
   exports: [FooterComponent],
 })
