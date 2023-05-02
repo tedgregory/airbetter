34305df09ff75123e5b8e528443d7f44b73e1b39
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-05-02 04:09:08 +0300

    feat: create booking-summary-flight cards

--- src/app/modules/booking/components/booking-summary-flight/booking-summary-flight.module.ts
+++ src/app/modules/booking/components/booking-summary-flight/booking-summary-flight.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingSummaryFlightComponent } from './booking-summary-flight.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [BookingSummaryFlightComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatDividerModule],
   exports: [BookingSummaryFlightComponent],
 })
 export class BookingSummaryFlightModule {}
