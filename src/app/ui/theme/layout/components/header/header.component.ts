ed527d8d2a80ca2dd51be4ce7f14a42c61feece1
Elena Markova <webkwondo.com@gmail.com>
2023-05-04 00:02:40 +0300

    feat: add header functionality and styling based on #10

--- src/app/ui/theme/layout/components/header/header.component.ts
+++ src/app/ui/theme/layout/components/header/header.component.ts
@@ -1,19 +1,140 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
 import { MatDialog } from '@angular/material/dialog';
 import { NavigationPath } from 'src/app/core/navigation/models/navigation.interface';
 import { HelpDialogComponent } from './components/help-dialog/help-dialog.component';
+import { ActivatedRoute } from '@angular/router';
+import {
+  AUTO_STYLE,
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import {
+  GridBreakpointType,
+  mediaBreakpointDown,
+} from '../../../utils/grid-breakpoints.util';
+import { Subject, takeUntil, tap } from 'rxjs';
+
+interface DateFormat {
+  value: string;
+  viewValue: string;
+}
+
+interface Currency {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  animations: [
+    trigger('collapse', [
+      state(
+        'false',
+        style({
+          transform: 'translateX(0)',
+          visibility: AUTO_STYLE,
+        })
+      ),
+      state(
+        'true',
+        style({
+          transform: 'translateX(-100%)',
+          visibility: 'hidden',
+        })
+      ),
+      transition('false => true', animate('300ms ease-in')),
+      transition('true => false', animate('300ms ease-out')),
+    ]),
+  ],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  @ViewChild('hamburgerToggleRef') hamburgerToggleRef:
+    | ElementRef<HTMLElement>
+    | undefined = undefined;
+
   // eslint-disable-next-line @typescript-eslint/naming-convention
   NavigationPath = NavigationPath;
 
-  constructor(private readonly matDialog: MatDialog) {}
+  isLgScreen = false;
 
-  onOpenDialog() {
+  collapse = true;
+
+  private readonly destroy$ = new Subject<void>();
+
+  dateFormats: DateFormat[] = [
+    { value: 'mm-dd-yyyy', viewValue: 'MM/DD/YYYY' },
+    { value: 'dd-mm-yyyy', viewValue: 'DD/MM/YYYY' },
+    { value: 'yyyy-dd-mm', viewValue: 'YYYY/DD/MM' },
+    { value: 'yyyy-mm-dd', viewValue: 'YYYY/MM/DD' },
+  ];
+
+  currencies: Currency[] = [
+    { value: 'eur', viewValue: 'EUR' },
+    { value: 'usa', viewValue: 'USA' },
+    { value: 'rub', viewValue: 'RUB' },
+    { value: 'pln', viewValue: 'PLN' },
+  ];
+
+  selectedDateFormat = this.dateFormats[0].value;
+  selectedCurrency = this.currencies[0].value;
+
+  constructor(
+    private route: ActivatedRoute,
+    private readonly breakpointObserver: BreakpointObserver,
+    private readonly matDialog: MatDialog
+  ) {}
+
+  ngOnInit() {
+    this.breakpointObserver
+      .observe(mediaBreakpointDown(GridBreakpointType.Lg))
+      .pipe(
+        tap((breakpoints) => {
+          this.isLgScreen = breakpoints.matches;
+          this.collapse = this.isLgScreen;
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  isFlightsPage() {
+    const currentRoute = this.route?.snapshot?.firstChild?.routeConfig?.path;
+    return currentRoute === NavigationPath.Flights;
+  }
+
+  onSingInBtnClick() {
+    // console.log(this.selectedDateFormat);
     this.matDialog.open(HelpDialogComponent);
+  }
+
+  toggleCollapse() {
+    this.collapse = !this.collapse;
+  }
+
+  clickedOutsideMenu(el: HTMLElement) {
+    if (
+      this.hamburgerToggleRef &&
+      el !== this.hamburgerToggleRef.nativeElement &&
+      !this.hamburgerToggleRef.nativeElement.contains(el) &&
+      !this.collapse
+    ) {
+      this.collapse = true;
+    }
   }
 }
--- src/app/ui/theme/layout/components/header/header.module.ts
+++ src/app/ui/theme/layout/components/header/header.module.ts
@@ -2,24 +2,32 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 import { ContainerModule } from 'src/app/ui/container/container.module';
 import { GridModule } from 'src/app/ui/grid/grid.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { HelpDialogModule } from './components/help-dialog/help-dialog.module';
 import { HeaderComponent } from './header.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LayoutDirectivesModule } from '../../directives/layout-directive.module';
 
 @NgModule({
   // imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, HelpDialogModule, MatDialogModule, NavigationSharedModule],
   imports: [
+    CommonModule,
     RouterModule,
     ContainerModule,
     GridModule,
-    MatToolbarModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
     MatButtonModule,
     MatIconModule,
     HelpDialogModule,
     MatDialogModule,
+    LayoutDirectivesModule,
   ],
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
