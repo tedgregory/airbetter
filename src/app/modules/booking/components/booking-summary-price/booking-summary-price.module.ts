8e22a1dc7420685623cefd967bb66eb199921ab2
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-05-04 01:38:14 +0300

    feat: create booking summary price card

--- src/app/modules/booking/components/booking-summary-price/booking-summary-price.module.ts
+++ src/app/modules/booking/components/booking-summary-price/booking-summary-price.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingSummaryPriceComponent } from './booking-summary-price.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [BookingSummaryPriceComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatDividerModule],
   exports: [BookingSummaryPriceComponent],
 })
 export class BookingSummaryPriceModule {}
