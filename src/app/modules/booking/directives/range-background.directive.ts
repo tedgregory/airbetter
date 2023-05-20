7a135117e9b664904a7305995c884c93070c3922
tedgregory <mail@tedgregory.ru>
2023-05-20 18:59:20 +0300

    fix: adjust rangebg directive

--- src/app/modules/booking/directives/range-background.directive.ts
+++ src/app/modules/booking/directives/range-background.directive.ts
@@ -9,21 +9,19 @@ import {
   selector: '[appRangeBackground]',
 })
 export class RangeBackgroundDirective implements AfterContentChecked {
-  @Input('appRangeBackground') value = 0;
-  @Input()
-  bgBreakpoints = [30, 20, 10];
+  @Input('appRangeBackground') value = [0, 0];
 
   constructor(private el: ElementRef<HTMLElement>) {}
 
   ngAfterContentChecked(): void {
     let className = 'seats--z';
-    if (this.value <= this.bgBreakpoints[0]) {
+    if (this.value[0] / this.value[1] >= 2) {
       className = 'seats--l';
     }
-    if (this.value <= this.bgBreakpoints[1]) {
+    if (this.value[0] / this.value[1] < 2) {
       className = 'seats--m';
     }
-    if (this.value <= this.bgBreakpoints[2]) {
+    if (this.value[1] < 10) {
       className = 'seats--s';
     }
     this.el.nativeElement.classList.add(className);
