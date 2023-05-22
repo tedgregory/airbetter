e8455af52ba337b6a87c3b1b5e3c10e296183554
Nebesnaya94 <kuroi.alice.13@gmail.com>
2023-05-23 01:04:41 +0300

    feat: create passenger form and validation

--- src/app/modules/booking/components/booking-passengers-card/booking-passengers-card.component.ts
+++ src/app/modules/booking/components/booking-passengers-card/booking-passengers-card.component.ts
@@ -1,7 +1,41 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { birthDateValidator } from '../../validators/birth-date-validator';
 
 @Component({
   selector: 'app-booking-passengers-card',
   templateUrl: './booking-passengers-card.component.html',
 })
-export class BookingPassengersCardComponent {}
+export class BookingPassengersCardComponent {
+  passengerForm = new FormGroup({
+    firstName: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z]{3,}'),
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[a-zA-Z]{3,}'),
+    ]),
+    gender: new FormControl('', [Validators.required]),
+    dateOfBirth: new FormControl('', [
+      Validators.required,
+      birthDateValidator(),
+    ]),
+    baggage: new FormGroup({
+      cabinBaggage: new FormControl(0, [Validators.min(0), Validators.max(3)]),
+      personalBaggage: new FormControl(0, [
+        Validators.min(0),
+        Validators.max(3),
+      ]),
+      checkedBaggage: new FormControl(0, [
+        Validators.min(0),
+        Validators.max(3),
+      ]),
+    }),
+  });
+
+  onSubmit() {
+    console.log(this.passengerForm);
+    console.log(this.passengerForm.valid);
+  }
+}
