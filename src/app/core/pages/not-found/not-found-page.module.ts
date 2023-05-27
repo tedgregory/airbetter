52c8bce43388d256b15b2ad5eb668817da6bea52
Elena Markova <webkwondo.com@gmail.com>
2023-05-28 02:31:00 +0300

    feat: add not found page contents based on #28

--- src/app/core/pages/not-found/not-found-page.module.ts
+++ src/app/core/pages/not-found/not-found-page.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-// import { NotFoundComponentsModule } from './components';
 import { NotFoundPageComponent } from './not-found-page.component';
 import { NotFoundPageRoutingModule } from './not-found-page-routing.module';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [CommonModule, NotFoundPageRoutingModule],
+  imports: [CommonModule, NotFoundPageRoutingModule, MatButtonModule],
   declarations: [NotFoundPageComponent],
 })
 export class NotFoundPageModule {}
